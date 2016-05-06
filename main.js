c23c5b0948994ea3a039742d7f7f5f2ce1fedaa3
Pawel Licznerski <pawellicznerski@poczta.fm>
2016-05-06 16:58:26 +0200

    Pierwszy commit

---
+++ main.js
@@ -0,0 +1,9 @@
+function hitTheLoop(){
+	for(i=0;i>=4;i++){
+	var newDiv = document.createElement("div");
+	var lista = getElementById("lista");
+	lista.appendChild(newDiv);
+	var text = document.createTextNode("Test");
+	newDiv.appendChild(text);  
+	}
+}
